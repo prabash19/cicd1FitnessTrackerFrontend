6156c228d4707acc1264abcfb197bf0ef6e27464
prabash19 <prabashdangi19@gmail.com>
2025-12-01 01:13:46 +1300

    merge conflict fixed

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,4 @@
 import { defineConfig } from "vitest/config";
-import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
